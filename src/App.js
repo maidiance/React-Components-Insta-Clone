6136082cd55b334255eaec28b09345355adb3448
Winifred Lu <winnie.lu@comcast.net>
2021-12-07 14:19:35 -0500

    app.js imports added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@
 // Import the state hook
 import React from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
 // Import the dummyData
+import dummyData from './dummy-data.js';
 import './App.css';
 
 const App = () => {
